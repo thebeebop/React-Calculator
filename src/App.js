879418fb56abe36585f474f371d9a10cf8246f8c
Lewis Woods <thebebop@The-Beebop.local>
2023-01-28 20:28:04 +0000

    y

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Calculator />
     </>
   );
