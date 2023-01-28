3d60dd01136fe207b4f2cc35a24283cff1eb3ef0
Lewis Woods <thebebop@The-Beebop.local>
2023-01-28 20:25:04 +0000

    remove header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Header from "./components/Header";
 import Calculator from "./components/Calculator";
 
 const App = () => {
   return (
     <>
-      <Header />
       <Calculator />
     </>
   );
